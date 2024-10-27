8e7ace06027c48805bf6f6b8ed2f735424effd3f
Mohammed Muzammil Anwar <muzammil@pluginhive.com>
2024-10-27 19:38:57 +0530

    update package.json to handle exports.

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,10 +4,11 @@ import { promisify } from 'util';
 
 const TSC_PATH = './node_modules/.bin/tsc';
 const FORMATS = ['cjs', 'esm'];
+const FILE_EXTENSIONS_MAP = { cjs: 'cjs', esm: 'mjs' };
 
 const main = async () => {
   await promisify(exec)(TSC_PATH).then(() => FORMATS.forEach((format) => build({
-    entryPoints: ['src/index.js'], bundle: true, outfile: `lib/index.${format}.js`, format, minify: true, target: 'esnext', logLevel: 'info', 
+    entryPoints: ['src/index.js'], bundle: true, outfile: `lib/index.${FILE_EXTENSIONS_MAP[format]}`, format, minify: true, target: 'esnext', logLevel: 'info',
   })));
 };
 
