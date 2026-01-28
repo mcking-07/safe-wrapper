7559b6967b566389dff314e60426065c3688f0af
Mohammed Muzammil Anwar <muzammil@pluginhive.com>
2026-01-29 00:53:11 +0530

    migrate to typescript.

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,20 @@ type Result<ResponseType, ErrorType> = [ErrorType, null] | [null, ResponseType];
 
 type Safe<ActionType extends (...args: unknown[]) => unknown, ErrorType = Error> = ReturnType<ActionType> extends Promise<infer ResponseType> ? (...args: Parameters<ActionType>) => Promise<Result<ResponseType, ErrorType>> : (...args: Parameters<ActionType>) => Result<ReturnType<ActionType>, ErrorType>;
 
+type ErrorConstructor = new (...args: any[]) => Error;
+
 const response = <ResponseType, ErrorType = Error>({ error, data }: { error?: ErrorType; data?: ResponseType; }): Result<ResponseType, ErrorType> => error ? [error as ErrorType, null] : [null, data as ResponseType];
 
-const promised = <PromiseType>(result: PromiseType): result is PromiseType & Promise<unknown> => result instanceof Promise || (typeof result === 'object' && result !== null && typeof (result as { then?: unknown }).then === 'function');
+const promised = <PromiseType>(result: PromiseType | PromiseLike<PromiseType>): result is Promise<PromiseType> => result instanceof Promise || (result !== null && typeof result === 'object' && 'then' in result && typeof result.then === 'function');
 
 const transform = <TransformedErrorType = Error>(transformer: (error: Error) => Promise<TransformedErrorType> | TransformedErrorType) => (error: Error): Promise<Result<null, TransformedErrorType>> | Result<null, TransformedErrorType> => {
-  try {
-    const transformed = transformer(error);
+  const transformed = transformer(error);
 
-    if (promised(transformed)) {
-      return transformed.then((value: unknown) => response<null, TransformedErrorType>({ error: value as TransformedErrorType })).catch((error: TransformedErrorType) => response({ error }));
-    }
-
-    return response({ error: transformed as TransformedErrorType });
-  } catch (error) {
-    return response({ error: error as TransformedErrorType });
+  if (promised(transformed)) {
+    return transformed.then((value) => response({ error: value }));
   }
+
+  return response({ error: transformed });
 };
 
 const caught = <ErrorType = Error>(error: Error, types: ErrorConstructor[] = [], transformer?: (error: Error) => Promise<ErrorType> | ErrorType): Promise<Result<null, ErrorType>> | Result<null, ErrorType> => {
@@ -31,12 +29,12 @@ const caught = <ErrorType = Error>(error: Error, types: ErrorConstructor[] = [],
   throw error;
 };
 
-const safe = <ActionType extends (...args: unknown[]) => unknown, ErrorType = Error>(action: ActionType, types: ErrorConstructor[] = [], transformer?: (error: Error) => Promise<ErrorType> | ErrorType): Safe<ActionType, ErrorType> => ((...args: Parameters<ActionType>) => {
+const safe = <ActionType extends (...args: any[]) => any, ErrorType = Error>(action: ActionType, types: ErrorConstructor[] = [], transformer?: (error: Error) => Promise<ErrorType> | ErrorType): Safe<ActionType, ErrorType> => ((...args: Parameters<ActionType>) => {
   try {
     const result = action(...args);
 
     if (promised(result)) {
-      return result.then((data: unknown): Result<ReturnType<ActionType>, Error> => response({ data: data as ReturnType<ActionType> })).catch((error: Error) => caught(error, types, transformer));
+      return result.then((data: ReturnType<ActionType>): Result<ReturnType<ActionType>, Error> => response({ data })).catch((error: Error) => caught(error, types, transformer));
     }
 
     return response({ data: result });
